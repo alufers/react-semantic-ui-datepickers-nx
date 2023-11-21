13363afc38f55a2ca28c84cbcf322b75e448b13f
alufers <alufers@wp.pl>
2023-11-21 23:48:12 +0100

    Update deps and storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['react-app', 'prettier', 'plugin:prettier/recommended'],
+  extends: ['react-app', 'prettier', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   rules: {
     '@typescript-eslint/consistent-type-assertions': 0,
     'react/button-has-type': [2, { submit: false, reset: false }],
--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,18 @@
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
   addons: ['@storybook/addon-knobs', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-storysource'],
+
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
+  },
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
   }
 };
